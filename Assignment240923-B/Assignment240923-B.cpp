79ce21152e81668f9921883218faaaebf81b7f2c
AssortRock <33kkm33@gmail.com>
2024-09-23 16:23:03 +0900

    1

--- Assignment240923-B/Assignment240923-B.cpp
+++ Assignment240923-B/Assignment240923-B.cpp
@@ -1,9 +1,66 @@
 ﻿#include <iostream>
 
+#define NUMBER_TO_ASCII(n) n + 0x30
 
+int GetDigit(int nInputData);
+int GetNumberAtDigit(int nInputData, int nTargetDigit);
+void NumberToString(char* lpszBuffer, int nSize, int nInputValue);
+
+int GetDigit(int nInputData)
+{
+    int nDigit{};
+
+    while (nInputData)
+    {
+        nInputData = nInputData / 10;
+        nDigit++;
+    }
+
+    return nDigit;
+}
+
+int GetNumberAtDigit(int nInputData, int nTargetDigit)
+{
+    int nResult = nInputData;
+
+    for (int i = 1; i < nTargetDigit; i++)
+    {
+        nResult = nResult / 10;
+    }
+
+    nResult = nResult % 10;
+
+    return nResult;
+}
+
+void NumberToString(char* lpszBuffer, const int nSize, const int nInputValue)
+{
+    const int nTotalDigit = GetDigit(nInputValue);
+    int nCurrentDigit = nTotalDigit;
+
+    for (int i = 0; i < nTotalDigit; i++)
+    {
+        nCurrentDigit = nTotalDigit - i;
+        lpszBuffer[i] = NUMBER_TO_ASCII(GetNumberAtDigit(nInputValue, nCurrentDigit));
+    }
+
+    return;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    // 5
+    int Result0 = GetDigit(12358);
+    // 4
+    int Result1 = GetDigit(5258);
+    // 7
+    int Result2 = GetDigit(5258111);
+
+    // 0은 영
+
+    char Buffer[100] = { 0 };
+
+    NumberToString(Buffer, 100, 3712);
+    // Buffer == "3712"
 }
 
