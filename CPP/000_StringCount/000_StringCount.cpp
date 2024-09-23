958595109196072072483a729beda33182587d90
chcjsdud 조해근 <chcjsdud@naver.com>
2024-09-23 15:48:56 +0900

    조해근 : 업데이트

--- CPP/000_StringCount/000_StringCount.cpp
+++ CPP/000_StringCount/000_StringCount.cpp
@@ -6,24 +6,37 @@
 // 지금만들라고 하는 함수들은
 // strlen
 
+
+
 int StringCount(const char* const _Str)
 {
+    //char Ch0 = _Str[0];
+    //char Ch1 = _Str[1];
+    //char Ch2 = _Str[2];
+    //char Ch3 = _Str[3];
+    //char Ch4 = _Str[4];
+
     int Count = 0;
 
-    while (_Str[Count])
+    while (true)
     {
-
+        char Ch = _Str[Count];
+        if (0 == Ch)
+        {
+            break;
+        }
+        Count += 1;
     }
 
-    return 0;
+    return Count;
 }
-
 int main()
 {
     // [B][B][B][B][0]
 
-    int Count = strlen("BBBB");
-    int MyCount = StringCount("BBBB");
+    // int Count = strlen("ABCD");
+    // ABCD0을 다 본다.
+    int MyCount = StringCount("ABCD");
 
     int a = 0;
 }
