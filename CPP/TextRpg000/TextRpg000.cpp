17149c94b9710bcd282fe302d236be1100f99501
chcjsdud 조해근 <chcjsdud@naver.com>
2024-09-24 16:24:50 +0900

    조해근 : 업데이트

--- CPP/TextRpg000/TextRpg000.cpp
+++ CPP/TextRpg000/TextRpg000.cpp
@@ -11,12 +11,15 @@ const int NAMELEN = 10;
 // 절차지향 
 // 어떠한 현실적인 개념을 컴퓨터 세계로 끌고 들어온다.
 // 상태와 
+int PlayerAttMin = 0;
 int PlayerAtt = 0;
 int PlayerHp = 0;
+int PlayerSpeed = 10;
 char PlayerName[NAMELEN] = "NONE";
 
 int MonsterAtt = 10;
 int MonsterHp = 100;
+int MonsterSpeed = 10;
 char MonsterName[NAMELEN] = "NONE";
 
 // 아주 중요한
@@ -41,20 +44,22 @@ void StrCopy(char* _Arr, int _BufferSize, const char* const _Name)
 // 전 많이 만들수록 좋아합니다.
 
 // 행동
-void CreatePlayer(const char* const _Ptr, int _Att, int _Hp)
+void CreatePlayer(const char* const _Ptr, int _Att, int _Hp, int _Speed)
 {
     // char[100] = 300번지
     StrCopy(PlayerName, NAMELEN,_Ptr);
     PlayerAtt = _Att;
     PlayerHp = _Hp;
+    PlayerSpeed = _Speed;
 }
 
 // 모든 문법은 코드를 덜치기 위해서 발전해왔습니다.
-void CreateMonster(const char* const _Ptr, int _Att, int _Hp)
+void CreateMonster(const char* const _Ptr, int _Att, int _Hp, int _Speed)
 {
     StrCopy(MonsterName, NAMELEN, _Ptr);
     MonsterAtt = _Att;
     MonsterHp = _Hp;
+    MonsterSpeed = _Speed;
 }
 
 void StatusRender(const char* _Name, int _Att, int _HP)
@@ -94,14 +99,22 @@ void MonsterStatusRender()
 // 함수는 작은 기능을 많이 만들고 
 // 함수는 한번에 1가지 일을 할수록 좋다.
 // 로직과 랜더를 분리해야 한다.
-void Damage(const char* const _AttName, const char* const _DefName, int& _DefHp, int _Att)
+void DamageRender(const char* const _AttName, const char* const _DefName, int _Att)
 {
     // 랜더링
     printf_s("%s 가 %s를 공격해서 %d의 데미지를 입혔습니다.\n", _AttName, _DefName, _Att);
+}
 
-    // 게임 로직
+void DamageLogic(int& _DefHp, int _Att)
+{
     _DefHp -= _Att;
 }
+
+void SpeedCheckRender(const char* const _Name)
+{
+    printf_s("%s 의 선공입니다\n", _Name);
+}
+
 
 int main()
 {
@@ -109,8 +122,15 @@ int main()
     /*char Test1[50] = Test0;
     Test1 = Test0*/;
 
-    CreatePlayer("1", 10, 100);
-    CreateMonster("Orc", 10, 50);
+    // 선공과 후공을 만드세요.
+    CreatePlayer("1", 10, 100, 20);
+    CreateMonster("Orc", 10, 100, 10);
+
+    // 플레이어는 0~19
+    // 몬스터는 0~9
+    // 플레이어와 몬스터는 각자 난수로 자신의 현재 스피드 능력
+    // 시작하기 전에 몬스터와 플레이어는 서로 현재 속력을 뽑아낸다.
+    // 더 높은 쪽이 먼저 공격한다.
 
     while (true)
     {
@@ -120,14 +140,30 @@ int main()
 
         char Input = ' ';
 
+
         PlayerStatusRender();
         MonsterStatusRender();
         Input = _getch();
 
-        Damage(PlayerName, MonsterName, MonsterHp, PlayerAtt);
+        system("cls");
+        // 데미지 주고
+        DamageLogic(MonsterHp, PlayerAtt);
+        // 다시 다 출력
+        PlayerStatusRender();
+        MonsterStatusRender();
+        SpeedCheckRender(PlayerName);
+        DamageRender(PlayerName, MonsterName, PlayerAtt);
         Input = _getch();
 
-        Damage(MonsterName, PlayerName, PlayerHp, MonsterAtt);
+        system("cls");
+        DamageLogic(PlayerHp, MonsterAtt);
+        // 다시 다 출력
+        PlayerStatusRender();
+        MonsterStatusRender();
+        SpeedCheckRender(PlayerName);
+        DamageRender(PlayerName, MonsterName, PlayerAtt);
+        DamageRender(MonsterName, PlayerName, MonsterAtt);
+
         Input = _getch();
     }
     
